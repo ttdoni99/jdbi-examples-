fc734d47606ba4331c6f6ce51e308ee739b00826
ttdoni99 <43473991+ttdoni99@users.noreply.github.com>
2020-04-12 11:46:36 +0200

    Add files via upload

---
+++ jdbi-examples/src/main/java/user/User.java
@@ -0,0 +1,27 @@
+package user;
+
+import java.time.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class User {
+
+    public static enum Gender {
+        FEMALE,
+        MALE
+    }
+
+    private Long id;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private Gender gender;
+    private LocalDate dob;
+    private boolean enabled;
+
+
+}
